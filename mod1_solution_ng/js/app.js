e399e2029a13cceaf3084055a51e5243f92b7310
ewkueffer <ewkueffer@sunrise.ch>
2017-02-19 16:24:36 +0100

    optional part with color set to messge and border added

--- mod1_solution_ng/js/app.js
+++ mod1_solution_ng/js/app.js
@@ -1,16 +1,16 @@
-(function () {
+(function () {   // by ewkueffer, 19.02.2017
 'use strict';
 
 angular.module('LunchCheck', [])
 .controller('LunchCheckController', LunchCheckController);
 
-LunchCheckController.$inject = ['$scope'];
+LunchCheckController.$inject = ['$scope']; // to protect from minification
 
 function LunchCheckController ($scope, $filter) {
   $scope.foodItems = "";
   $scope.checkTooMuch = function(){
     var foodString = $scope.foodItems;
-    console.log("This is the list got:" + foodString);
+    console.log("This is the list got: [" + foodString + "]");
 
     var foodArray = foodString.split(',');
     var filteredFoodArray = [];
@@ -19,28 +19,29 @@ function LunchCheckController ($scope, $filter) {
         filteredFoodArray.push(item.trim());
       }
     });
-    console.log(filteredFoodArray + "  " + filteredFoodArray.length);
+    $scope.msgColor = "green";
+    console.log(filteredFoodArray + " length: " + filteredFoodArray.length);
     if ( filteredFoodArray.length > 3){
       $scope.resultMsg = "Too much!";
     } else if ( filteredFoodArray.length > 0){
       $scope.resultMsg = "Enjoy!";
     } else { // no element
       $scope.resultMsg = "Please enter data first";
+      $scope.msgColor = "red";
     }
     $scope.noteMsg = " ";
     if(foodArray.length != filteredFoodArray.length){
-      console.log("found" + foodArray.lenth + "  " + filteredFoodArray.length);
+      console.log("items found: " + foodArray.length + " considering: " + filteredFoodArray.length);
       $scope.noteMsg = "Please note that empty items will be ignored!";
     }
     console.log($scope.resultMsg);
   }
 
-  // $scope.name = "Yaakov";
-  //
-  // $scope.upper = function () {
-  //   var upCase = $filter('uppercase');
-  //   $scope.name = upCase($scope.name);
-  // };
+  $scope.resetLunchCheck = function(){
+    $scope.msgColor = "";
+    $scope.noteMsg = "";
+    $scope.resultMsg = "";
+  }
 }
 
 })();
